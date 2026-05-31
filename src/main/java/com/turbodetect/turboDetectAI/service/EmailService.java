41a0c3930bbccb09f34c590e48d74ce5586b8d8e
ifteakar <rony00kg@gmail.com>
2026-05-31 10:03:26 +0600

    fix: hardcoded actual sender email for brevo

--- src/main/java/com/turbodetect/turboDetectAI/service/EmailService.java
+++ src/main/java/com/turbodetect/turboDetectAI/service/EmailService.java
@@ -1,7 +1,6 @@
 package com.turbodetect.turboDetectAI.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,14 +11,12 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
-    private String fromEmail;
-
     public void sendVerificationCode(String toEmail, String code) {
 
         SimpleMailMessage message = new SimpleMailMessage();
 
-        message.setFrom(fromEmail);
+        message.setFrom("rony00kg@gmail.com");
+
         message.setTo(toEmail);
         message.setSubject("TurboDetect AI - Verification Code");
 
